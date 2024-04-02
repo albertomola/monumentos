cf2cf02ff64cfcf3457221a4bc4a6ba9cc70ea32
albertomola <pacmonkeykong@gmail.com>
2024-04-02 06:51:40 -0700

    Initial Commit

---
+++ comida/comidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  comidaApp.swift
+//  comida
+//
+//  Created by alberto on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct comidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
